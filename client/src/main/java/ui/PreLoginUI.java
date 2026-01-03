95fe589535dceabcdacb0dcb7fd59ba7d7cf1f72
TayziaS <taycat21@gmail.com>
2026-01-03 13:59:31 -0700

    add ServerFacade file

--- client/src/main/java/ui/PreLoginUI.java
+++ client/src/main/java/ui/PreLoginUI.java
@@ -1,4 +1,39 @@
 package ui;
 
+import client.ServerFacade;
+
 public class PreLoginUI {
+
+    private String visitorName = null;
+    private final ServerFacade server;
+    private final WebSocketFacade ws;
+    private State state = State.SIGNEDOUT;
+
+    public PetClient(String serverUrl) throws ResponseException {
+        server = new ServerFacade(serverUrl);
+        ws = new WebSocketFacade(serverUrl, this);
+    }
+
+    public void run() {
+        System.out.println(LOGO + " Welcome to the pet store. Sign in to start.");
+        System.out.print(help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.print(BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+
 }
