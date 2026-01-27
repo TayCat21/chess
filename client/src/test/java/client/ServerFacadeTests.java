b464753af319de1af89be9856ffcfe9a02c3b282
TayziaS <taycat21@gmail.com>
2026-01-27 16:12:01 -0700

    code Quality 3

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -14,9 +14,9 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        facade = new ServerFacade("http://localhost:8080");
 
         var port = server.run(8080);
+        facade = new ServerFacade("http://localhost:" + port);
         System.out.println("Started test HTTP server on " + port);
     }
 
